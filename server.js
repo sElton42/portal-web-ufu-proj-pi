d9cf9082ce884727cf2ea9bfe656684e89907d22
Elton Soares <elton.tell@gmail.com>
2022-03-28 18:14:00 -0300

    ATT14

--- server.js
+++ server.js
@@ -7,16 +7,16 @@ const { resolve, dirname } = require('path')
 
 app.use(cors())
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
 
-app.post("/", (req, res) => {
-    // perform operation and return response.
-    res.status(200).json({ message: "It worked!" });
-  });
+// app.post("/", (req, res) => {
+//     // perform operation and return response.
+//     res.status(200).json({ message: "It worked!" });
+//   });
   
   app.listen(3000, function() {
       console.log("server is running");
